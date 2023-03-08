ea8eb244bf37cbeb478844903aea1dee491359d6
Jagroop Singh <jsingh8448@gmail.com>
2023-03-08 01:16:48 -0800

    Implement writing insert queries

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::io::Write;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::path::PathBuf;
+use itertools::Itertools;
+
 use md5;
 use clap::Parser;
 use nom::{
-    bytes::complete::{is_not, tag, take_till},
-    IResult,
+    bytes::complete::{is_not, tag, take_till}, IResult,
     combinator::map_res,
 };
 
@@ -26,29 +27,24 @@ struct Cli{
     #[arg(short='w',long)]
     wordlist: Vec<String>,
 
+    #[arg(short='d',long)]
+    dbms: Option<String>,
+
     #[arg(short='o',long)]
     output: Option<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Constraint{
     NotNull,
-    Unique,
+    UNIQUE,
     PrimaryKey,
-    Check(Checks),
+    CHECK(String),
     Default(String),
     Empty,
 }
 
-#[derive(Debug)]
-enum Checks{
-    GreaterThan,
-    LessThan,
-    EqualTo,
-    InBetween,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Transformations{
     Empty,
     MD5,
@@ -58,97 +54,139 @@ enum Transformations{
     SHA512,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Value{
     name: String,
     data_type: String,
     constraint: Constraint,
 }
 
-struct Values{
-    values: Vec<Value>,
-}
-
 #[derive(Debug)]
 struct Wordlist{
-    value: String,
+    value: Value,
     wordlist: String,
     transformation: Transformations,
 }
 
-fn parse_values(input: &str)->IResult<&str,Value>{
+fn parse_values<'a>(input: &'a str, values: &mut Vec<Value>)->IResult<&'a str,Value>{
     let (input, name) = nom::bytes::complete::is_not(":")(input)?;
     let (input, _ ) = tag(":")(input)?;
     let (input,data_type) =  nom::bytes::complete::is_not(":")(input)?;
-    if input == ""{
+    let constraint  = match input.to_lowercase().as_str(){
+            ":not null" => Constraint::NotNull,
+            ":unique" => Constraint::UNIQUE,
+            ":primary key" => Constraint::PrimaryKey,
+            x if x.contains(":chec") => Constraint::CHECK(x.to_string()),
+            ":" | "" => Constraint::Empty,
+            _ => panic!("Constraint Not Allowed"),
+    };
+    
+    values.push(
+Value{
+            name:name.to_string(),
+            data_type:data_type.to_string(),
+            constraint:constraint.clone(),
+        }
+    );
     Ok(("",Value{
             name:name.to_string(),
             data_type:data_type.to_string(),
-            constraint:Constraint::Empty
+            constraint:constraint.clone(),
         }))
-    }
-    else{
-    let (input,_) = tag(":")(input)?;
-    Ok(("",Value{
-            name:name.to_string(),
-            data_type:data_type.to_string(),
-            constraint:Constraint::Default(input.to_string())
-        }))
-    }
 }
 
-fn parse_wordlist(input: &str) -> IResult<&str,Wordlist>{
+fn parse_wordlist<'a>(input: &'a str, values: &Vec<Value>, wordlists: &mut Vec<Wordlist>) -> IResult<&'a str,Wordlist>{
     let (input, value) = nom::bytes::complete::is_not(":")(input)?;
+    let mut i = 0;
+    let value:Value = loop{
+        if i>=values.len(){
+            panic!("invalid value in wordlist");
+        }
+       if value == values[i].name{
+            break values[i].clone();
+        }
+        i+=1;
+    };
+
     let (input, _ ) = tag(":")(input)?;
     let (input,wordlist) =  nom::bytes::complete::is_not(":")(input)?;
-    if input == ""{
-    Ok(("",Wordlist{
-            value:value.to_string(),
-            wordlist:wordlist.to_string(),
-            transformation:Transformations::Empty
-        }))
-    }
-    else{
-    let (input,_) = tag(":")(input)?;
     let input = match input.to_lowercase().as_str(){
-            "md5" => Transformations::MD5,
-            "md4" => Transformations::MD4,
-            "sha1" => Transformations::SHA1,
-            "sha256" => Transformations::SHA256,
-            "sha512" => Transformations::SHA512,
+            ":md5" => Transformations::MD5,
+            ":md4" => Transformations::MD4,
+            ":sha1" => Transformations::SHA1,
+            ":sha256" => Transformations::SHA256,
+            ":sha512" => Transformations::SHA512,
+            ":" | "" => Transformations::Empty,
             _ =>  panic!("Transformation not allowed"),
         };
-    Ok(("",Wordlist{
-            value:value.to_string(),
+    wordlists.push(Wordlist{
+            value:value.clone(),
             wordlist:wordlist.to_string(),
-            transformation:input,
+            transformation:input.clone(),
+        });
+    Ok(("",Wordlist{
+            value:value.clone(),
+            wordlist:wordlist.to_string(),
+            transformation:input.clone(),
         }))
-    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let cli = Cli::parse();
-    println!("\n\ntable: {:?}, values: {:?}, wordlist: {:?}, output: {:?}", cli.table, cli.values, cli.wordlist, cli.output);
+    // println!("\n\ntable: {:?}, values: {:?}, wordlist: {:?}, output: {:?}", cli.table, cli.values, cli.wordlist, cli.output);
 
-    println!("wordlist: {:?}", parse_wordlist(&cli.wordlist[0]));
-    for x in cli.values.iter(){
-        println!("value: {:?}", parse_values(x));
+    // Check Conditions
+    if cli.wordlist.len() != cli.values.len(){
+        panic!("Need a wordlist for each value and vice versa");
+    }
+
+    let mut values: Vec<Value> = Vec::new();
+
+    for x in 0..cli.values.len(){
+        parse_values(&cli.values[x], &mut values);
+    }
+    let mut wordlists: Vec<Wordlist> = Vec::new();
+    for x in cli.wordlist.iter(){
+        parse_wordlist(x, &values, &mut wordlists);
     };
-    // TODO! 
-    // Create State Machine for sql table creation
+    let mut output = File::create("results.txt")?;
 
-    // println!("wodlists: {:?}", cli.wordlist);
-    // let f = File::open("usernames.txt")?;
-    // let path = "results.txt";
-    // let mut output = File::create(path)?;
-    // let bufr = BufReader::new(f);
-    // let k:u32 = bufr.lines().map(|l|{
-    //     let line = l.unwrap();
-    //     write!(&mut output, "insert into users(username,password) values('{}','{}');\n",line,encrypt_md5(&line));
-    //     1
-    // }).sum();
-    // 
+    let mut files: Vec<_> = wordlists.iter()
+    .map(|wordlist| BufReader::new(File::open(wordlist.wordlist.as_str()).unwrap()).lines()).collect();
+    println!("{:?}", files);
+
+
+    // Todo! 
+    // 1. Write create table query
+    // 2. Implement reading from table name and writing to 
+    // optional output file 
+    // 3. Implement writing query in different dbms language
+
+    // Write insert statements
+    let mut pre_arg = String::new();
+    pre_arg.push_str("insert into users values(");
+    // Insert into fields
+    for i in 0..values.len(){
+        pre_arg.push_str(&("'".to_owned() + values[i].name.as_str() + "'"));
+        if i != values.len() -1{
+            pre_arg.push_str(",");
+        }
+    }
+    pre_arg.push_str(") values(");
+
+    while let Some(line) = files[0].next(){
+        write!(&mut output, "{}",pre_arg,);
+        write!(&mut output, "'{}'", line.unwrap());
+        for k in 1..files.len(){
+            write!(&mut output, "'{}'", files[k].next().unwrap().unwrap());
+            if k != files.len()-1{
+            write!(&mut output, ",");
+            }
+        }
+        write!(&mut output, ");\n");
+    }
+
     Ok(())
 }
 
